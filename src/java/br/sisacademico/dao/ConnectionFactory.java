813a96505202da04849ba64a5b51a989bcfc632b
Wennis <>
2022-06-07 02:00:51 -0300

    first commit

---
+++ src/java/br/sisacademico/dao/ConnectionFactory.java
@@ -0,0 +1,17 @@
+package br.sisacademico.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+ 
+    private static final String urlDB = "jdbc:derby://localhost:1527/test";
+    private static final String user = "root";
+    private static final String pass = "root";
+    
+    public static Connection getConnection() throws SQLException{
+        DriverManager.registerDriver(new org.apache.derby.jdbc.ClientDriver());
+        return DriverManager.getConnection(urlDB, user, pass);
+    }
+}
---
+++ src/java/br/sisacademico/util/AcaoDao.java
@@ -0,0 +1,9 @@
+package br.sisacademico.util;
+
+public enum AcaoDao {
+    CADASTRO,     //C (CREATE)
+    LEITURA,      //R (READ)
+    EDICAO,       //U (UPDATE)
+    EXCLUSAO,     //D (DELETE)
+    CARREGAMENTO;
+}
